5866eb354679b0da4a5a2b199fa1a694d9f7b5ae
Ismael Sh <me@theiskaa.com>
2024-12-20 00:15:28 +0400

    feat(models): add synthesizer response model

--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,12 @@
+//! Models for managing user state and theme creation
+//!
+//! This module contains the core data structures used to track user state
+//! during theme creation conversations with the bot. It manages which platform
+//! a user is creating a theme for and their theme description input.
+//!
+
+#![allow(dead_code)]
+
 use std::collections::HashMap;
 use std::sync::Arc;
 use teloxide::types::UserId;
@@ -10,4 +19,10 @@ pub type UserStates = Arc<Mutex<HashMap<UserId, UserState>>>;
 pub struct UserState {
     pub platform: Option<String>,
     pub theme_description: Option<String>,
+}
+
+#[derive(serde::Deserialize)]
+pub struct SynthesizerOutput {
+    pub theme: String,
+    pub description: String,
 }
